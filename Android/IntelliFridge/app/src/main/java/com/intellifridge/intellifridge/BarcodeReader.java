812ac6ed2bb9884425d85b2ffb2111e6a80b9b11
Francis Makokha <francisomakokha@outlook.com>
2016-10-05 19:14:50 +0200

    Added barcode reader

---
+++ Android/IntelliFridge/app/src/main/java/com/intellifridge/intellifridge/BarcodeReader.java
@@ -0,0 +1,50 @@
+package com.intellifridge.intellifridge;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class BarcodeReader extends AppCompatActivity implements View.OnClickListener {
+    private Button scanBtn;
+    private TextView formatTxt, contextTxt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barcode_reader);
+
+        scanBtn = (Button)findViewById(R.id.scan_button);
+        formatTxt = (TextView)findViewById(R.id.scan_format);
+        contextTxt = (TextView)findViewById(R.id.scan_content);
+
+        scanBtn.setOnClickListener(this);
+    }
+
+    public void onClick(View view){
+        if(view.getId()==R.id.scan_button){
+            IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+            scanIntegrator.initiateScan();
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent){
+        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+
+        if (scanningResult != null){
+            String scanContent = scanningResult.getContents();
+            String scanFormat = scanningResult.getFormatName();
+            formatTxt.setText("Format: " + scanFormat);
+            contextTxt.setText("Content: " + scanContent);
+        }else {
+            Toast.makeText(getApplicationContext(), "No Scan Data Received", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
